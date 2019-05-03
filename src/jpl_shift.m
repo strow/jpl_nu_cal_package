1b1d1ab0ad496b5238477622a9d492a01ce9a89b
L. Larrabee Strow <strow@umbc.edu>
2019-05-03 13:58:00 -0400

    Divide by dv=0 in jpl_shift for fill chans, making them NaN's, fixed

--- src/jpl_shift.m
+++ src/jpl_shift.m
@@ -14,5 +14,12 @@ v_nom = v_nom(:);
 
 dv = v_nom - v_in;
 Tb_spline = interp1(v_in, Tb_in, v_nom, 'pchip');
-Tb_resamp = Tb_in + (a .* (Tb_spline - Tb_in) ./ dv + b) .* dv;
+
+% Trap fill channels since dv == 0
+kf = dv == 0;
+k  = dv ~= 0;
+
+% Put back input for fill channels.
+Tb_resamp(k) = Tb_in(k) + (a(k) .* (Tb_spline(k) - Tb_in(k)) ./ dv(k) + b(k)) .* dv(k);
+Tb_resamp(kf) = Tb_in(kf);
 
