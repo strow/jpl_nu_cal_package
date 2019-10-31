a64d308aafce797044a3617c1eef65017b151946
L. Larrabee Strow <strow@umbc.edu>
2019-10-31 16:01:21 -0400

    Now doing 'spline' instead of 'pchip' in interp1
    Previous commit was what I did for testing previous to March 25, 2019.
    This previous commit uses csape to do the spline fitting, and it gets
    the BT derivative from the spline fit, instead of as a finite
    difference from the data.   So, I needed to test using interp1('spline')
    instead of csape.  I tested this on m3, m10, m12, all m4* arrays (much
    more than before) and all looks good.  The differences between csape
    and interp1('spline') are << 0.01 um in yoff units.  Basically identical.
    I want to use interp1('spline') intead of csape since interp1('spline')
    is ~3x faster (no derivative calcs).
    
    Note that all yoff values were measured by interpolating simulated BT's
    with interp1(using 'spline', NOT 'pchip').  In addition, the linear
    fit corrections (a,b coefficients) were derived using interp1('spline').
    csape was never used for any of these exercises.

--- src/jpl_shift.m
+++ src/jpl_shift.m
@@ -13,7 +13,7 @@ v_in = v_in(:);
 v_nom = v_nom(:);
 
 dv = v_nom - v_in;
-Tb_spline = interp1(v_in, Tb_in, v_nom, 'pchip');
+Tb_spline = interp1(v_in, Tb_in, v_nom, 'spline');
 
 % Trap fill channels since dv == 0
 kf = dv == 0;
@@ -24,18 +24,6 @@ k  = dv ~= 0;
 % Tb_resamp(k) = Tb_in(k) + (a(k) .* (Tb_spline(k) - Tb_in(k)) ./ dv(k) + b(k)) .* dv(k);
 
 % % At JPL for testing in March 25, 2019: WRONG
-% x1Tb_resamp(k) = Tb_in(k) + a(k) .* (Tb_spline(k) - Tb_in(k))  + b(k) .* dv(k);
-% x1Tb_resamp(kf) = Tb_in(kf);
-
-% Original tested approach (changed in git on March 25, 2019 to the above, from the below)
-% If no many points, punt, scape needs enough points to do a spline, 20 pts is arbitrary
-if length(find(k) == 1) < 20
-   Tb_resamp = Tb_in;
-else
-   pp = csape(v_in(k),Tb_in(k));
-   fprime = fnder(pp);
-   btderiv = fnval(fprime,v_nom);
-   Tb_resamp(k) = Tb_in(k) + (a(k).*btderiv(k) + b(k)).*dv(k);
-   Tb_resamp(kf) = Tb_in(kf);
-end
+Tb_resamp(k) = Tb_in(k) + a(k) .* (Tb_spline(k) - Tb_in(k))  + b(k) .* dv(k);
+Tb_resamp(kf) = Tb_in(kf);
 
